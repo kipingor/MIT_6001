7fee1dbc46c711226036bdfdf8f23fa0a7cd06d2
Antony Kipingor <kipingor@otadev.com>
2018-01-03 11:38:58 +0300

    Update COUNTING BOBS.py

--- COUNTING BOBS.py
+++ COUNTING BOBS.py
@@ -3,7 +3,6 @@
 # Write a program that prints the number of times the string 'bob' occurs in s.
 # For example, if s = 'azcbobobegghakl', then your program should print
 
-s = "azcbobobegghakl"
 j = 0
 for i in range(len(s)):
     if s[i:].startswith('bob'):

bca63082d5311842b6f7e761f5b4ab031387e277
Antony Kipingor <kipingor@otadev.com>
2018-01-03 11:36:59 +0300

    Update ALPHABETICAL SUBSTRINGS.py

--- ALPHABETICAL SUBSTRINGS.py
+++ ALPHABETICAL SUBSTRINGS.py
@@ -6,18 +6,15 @@
 #In the case of ties, print the first substring.
 #For example, if s = 'abcbcd', then your program should print "Longest substring in alphabetical order is: abc"
 
-
 from itertools import count
 
-s = "jdisjsd sdpkds sdpsdsdfposodksdpfok "
-
-maxSubstr = s[0:0]
-for start in range(len(s)):
-    for end in count(start + len(maxSubstr) + 1):
-        substr = s[start:end]
-        if len(substr) != (end - start):
+j = s[0:0]
+for i in range(len(s)):
+    for k in count(i + len(j) + 1):
+        l = s[i:k]
+        if len(l) != (k - i):
             break
-        if sorted(substr) == list(substr):
-            maxSubstr = substr
+        if sorted(l) == list(l):
+            j = l
 
-print "Longest substring in alphabetical order is: " + str(maxSubstr)
+print("Longest substring in alphabetical order is: " + str(j))

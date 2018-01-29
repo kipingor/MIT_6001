edc6ff2926d05c84942a717014d5d61a1802963e
Antony Kipingor <kipingor@otadev.com>
2018-01-29 08:31:59 +0300

    Update L3 PROBLEM 9.py

--- L3 PROBLEM 9.py
+++ L3 PROBLEM 9.py
@@ -5,23 +5,22 @@
 # The computer makes guesses, and you give it input - is its guess too high or too low? Using bisection search, the computer will guess the user's secret number!
 
 
-minNum = 0
-midNum = 50
-maxNum = 100
-guess = "Is your secret number: "
-print"Please think of a number between 0 and 100!"
-print guess + str(midNum) + "?"
-request = raw_input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
-while request != 'c':
-    if  request == 'h':
-        maxNum = midNum
-        midNum = int((midNum + minNum)/2)
-    elif request == 'l':
-        minNum = midNum
-        midNum = int((midNum + maxNum)/2)
+x = 100
+numGuesses = 0
+low = 0
+high = x
+ans = int((high + low)/2)
+print("is your secret number", str(ans) + "?")
+guess = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+while guess != 'c':
+    if guess == 'h':
+        high = ans
+    elif guess == 'l':
+        low = ans
     else:
-        print"Sorry, I did not understand your input."
-    print guess + str(midNum) + "?"
-    request = raw_input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
-if request == 'c':
-    print "Game over. Your secret number was:" + str(midNum)
+        print("Sorry, I did not understand your input.")
+    ans = int((high + low) / 2)
+    print("is your secret number", str(ans) + "?")
+    guess = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+if guess == 'c':
+    print("Game over. Your secret number was:", str(ans))
